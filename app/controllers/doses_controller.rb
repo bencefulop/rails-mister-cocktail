28b0f5195130e3ea1efa1222a1207b3bf2936087
Bence Fulop <fbence90@gmail.com>
2020-09-03 16:48:53 +1000

    trying to get the save doses work

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -6,7 +6,18 @@ class DosesController < ApplicationController
   end
 
   def create
-    raise
-    @dose = Dose.new
+    @dose = Dose.new(dose_params)
+    # @dose.ingredients = Ingredients.find(params[:dose][:ingredient_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    
+    @dose.save
+
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
