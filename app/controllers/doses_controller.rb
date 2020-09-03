4b80f90b87a50c66a2216c0db0c4ea5f71fd37f0
Bence Fulop <fbence90@gmail.com>
2020-09-03 16:29:59 +1000

    cocktail controller actions w view, started on doses controller and actions

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,12 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    raise
+    @dose = Dose.new
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Cleaning database"
+
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+
+puts "creating a 100 ingredients"
+n = 0
+100.times do
+  new_ingredient = Ingredient.new(
+    name: ingredient["drinks"][n]["strIngredient1"],
+  )
+  new_ingredient.save
+  n += 1
+end
+
+puts "100 ingredients created"
